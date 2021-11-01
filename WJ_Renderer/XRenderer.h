90b4dadcb30a8764b4aaa2c066ca108168702871
LeeWoojin <dnwls6613@naver.com>
2021-11-01 17:10:44 +0900

    Update XRenderer.h

--- WJ_Renderer/XRenderer.h
+++ WJ_Renderer/XRenderer.h
@@ -1,12 +1,16 @@
 #pragma once
-
+/// <summary>
+/// 렌더러의 기능을 지닌 인터페이스이다.
+/// </summary>
 __interface IRenderableFunc
 {
 	bool Render_Begin ()=0;
 	bool Render_Update()=0;
 	bool Render_End	  ()=0;
 };
-
+/// <summary>
+/// 이제 어떻게 그릴것인가..
+/// </summary>
 class XRenderer : public IRenderableFunc
 {
 public:
