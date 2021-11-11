ca905a17218d948ecba665605ab0c439f750b30d
abccc0623 <abccc0623@naver.com>
2021-11-11 19:04:36 +0900

    UpLoad
    스키닝 완료
    애니메이션 하는중

--- EaterEngine/EaterEngine/Animation.h
+++ EaterEngine/EaterEngine/Animation.h
@@ -4,13 +4,21 @@
 /// 본에 애니메이션을 돌리기위해 본마다 들어있는 컨퍼넌트 
 /// </summary>
 
+namespace ParserData
+{
+	struct OneAnimation;
+}
+
+
 #include "Component.h"
 class Animation : public Component
 {
 public:
 	void Update();
-private:
 
+	void SetAnimation(ParserData::OneAnimation*);
+private:
+	ParserData::OneAnimation* AnimationData;
 
 
 };
