b57108b9875acef5ada789b4ca166a024930da1d
LeeWoojin <dnwls6613@naver.com>
2021-11-25 17:46:59 +0900

    gg

--- GameClient/intro.cpp
+++ GameClient/intro.cpp
@@ -29,7 +29,7 @@ void intro::Awake()
 	//LoadMesh("Skinning");
 	LoadMesh("Enemy_Run",false,false);
 	LoadMesh("Inside_dome");
-	LoadMesh("map");
+	LoadMesh("Dome_v03");
 	//LoadMesh("Enemy_Roll",false,true);
 	//LoadMesh("Anim_Run",false,false);
 	//LoadMesh("Anim_Idle",false,true);
@@ -71,7 +71,7 @@ void intro::Awake()
 	Mf = testobj->AddComponent<MeshFilter>();
 	ac = nullptr;
 	tr = testobj->GetTransform();
-	Mf->SetMeshName("map");
+	Mf->SetMeshName("Inside_dome");
 	Mf->SetTextureName("MainTexture");
 	tr->Rotation = { 90 ,0,0 };
 
@@ -81,7 +81,7 @@ void intro::Awake()
 	Mf = testobj->AddComponent<MeshFilter>();
 	ac = nullptr;
 	tr = testobj->GetTransform();
-	Mf->SetMeshName("Inside_dome");
+	Mf->SetMeshName("Dome_v03");
 	Mf->SetTextureName("MainTexture");
 	tr->Rotation = { 90 ,0,0 };
 }
